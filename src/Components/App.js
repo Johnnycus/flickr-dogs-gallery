00f42022f7103c9fe06559f8b16b3141d3b7a3b8
Albert Itayev <sirjsx@gmail.com>
2018-12-14 23:55:45 +0100

    Finish main features

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Dogs from './Dogs'
+import Header from './Header'
 
-const App = () => <Dogs />
+const App = () => (
+  <Fragment>
+    <Header />
+    <Dogs />
+  </Fragment>
+)
 
 export default App
--- src/Components/Dogs.js
+++ src/Components/Dogs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import LinearProgress from '@material-ui/core/LinearProgress'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
@@ -9,6 +10,9 @@ import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = {
+  grid: {
+    marginTop: 75,
+  },
   card: {
     maxWidth: 300,
     margin: 20,
@@ -27,36 +31,76 @@ const styles = {
     left: 0,
     right: 0,
   },
+  photosLoader: {
+    width: '250px !important',
+    height: '250px !important',
+    margin: '-300px auto 0',
+    position: 'absolute',
+    right: 0,
+    left: 0,
+    opacity: 0.5,
+  },
 }
 
 class Dogs extends Component {
-  state = {
-    photos: [],
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: false,
+      isLoading: false,
+      photos: [],
+      loadTime: 1,
+    }
+
+    window.onscroll = () => {
+      const {
+        loadDogs,
+        state: { error, isLoading },
+      } = this
+
+      if (error || isLoading) return
+
+      if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+        loadDogs()
+      }
+    }
   }
 
-  componentDidMount() {
-    const api =
-      'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=888315f85320878f33df15f17795be6e&tags=dog&media=photos&extras=owner_name%2C+date_upload%2C+description%2C+original_format&format=json&nojsoncallback=1'
+  componentWillMount() {
+    this.loadDogs()
+  }
 
-    axios
-      .get(api, {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      })
-      .then(response => {
-        this.setState({
-          photos: response.data.photos.photo,
+  loadDogs = () => {
+    const api = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=888315f85320878f33df15f17795be6e&tags=dog&media=photos&extras=owner_name%2C+date_upload%2C+description%2C+original_format&format=json&nojsoncallback=1&page=${
+      this.state.loadTime
+    }`
+
+    this.setState({ isLoading: true }, () => {
+      axios
+        .get(api, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
         })
-      })
-      .catch(err => {
-        console.log(err)
-      })
+        .then(response => {
+          const morePhotos = response.data.photos.photo
+          this.setState({
+            isLoading: false,
+            photos: [...this.state.photos, ...morePhotos],
+            loadTime: this.state.loadTime + 1,
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          this.setState({ error: err.message, isLoading: false })
+        })
+    })
   }
 
   render() {
-    const { photos } = this.state
+    const { error, isLoading, photos } = this.state
     const { classes } = this.props
 
     const DogsList = ({ title, ownername, description, dateupload, farm, server, id, secret, original_format }) => {
@@ -65,7 +109,7 @@ class Dogs extends Component {
       }`
 
       return (
-        <Grid item xs={3}>
+        <Grid item xm={3} className={classes.grid}>
           <Card className={classes.card}>
             <CardContent>
               <Typography className={classes.title} color="textSecondary" gutterBottom>
@@ -84,11 +128,15 @@ class Dogs extends Component {
         {photos.length === 0 ? (
           <LinearProgress className={classes.loader} color="secondary" />
         ) : (
-          <Grid container direction="row" justify="space-around" alignItems="center">
-            {photos.map(photo => (
-              <DogsList key={photo.id} {...photo} />
-            ))}
-          </Grid>
+          <div>
+            <Grid container direction="row" justify="space-around" alignItems="center">
+              {photos.map(photo => (
+                <DogsList key={photo.id} {...photo} />
+              ))}
+            </Grid>
+            {error && <div>{error}</div>}
+            {isLoading && <CircularProgress className={classes.photosLoader} color="secondary" />}
+          </div>
         )}
       </div>
     )
